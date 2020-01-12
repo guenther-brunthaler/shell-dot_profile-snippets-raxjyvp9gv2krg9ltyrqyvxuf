8cbca229189a352221132c732c09fce05b2bf3b0
Guenther Brunthaler <root@xduo.emgenxx69lwyn5ctlr4nl64ul.local>
2020-01-12 02:26:40 +0100

    Migrate site recommendation loader

--- profile.d.avail/10site_recommendations.sh
+++ profile.d.avail/10site_recommendations.sh
@@ -1,14 +1,14 @@
 #! /bin/false
 # Source all the shell snippets recommended by the local site's administrator
 # for the current user. Those snippets are a subset (and just symlinks to)
-# those which can be found in ~/.profile.d/site/profile.d.avail/. If you do
+# those which can be found in ~/.profile.d/shellrc/profile.d.avail/. If you do
 # not want the administrator's chosen whole selection, symlink the individual
-# snippets from ~/.profile.d/site/profile.d.avail/ into your ~/.profile.d
+# snippets from ~/.profile.d/shellrc/profile.d.avail/ into your ~/.profile.d
 # directly instead.
 #
-# Version 2019.287
+# Version 2020.12
 
-for sh in ~/.profile.d/site/profile.d.recommended/*.sh
+for sh in ~/.profile.d/shellrc/profile.d.recommended/*.sh
 do
 	. "$sh"
 done
