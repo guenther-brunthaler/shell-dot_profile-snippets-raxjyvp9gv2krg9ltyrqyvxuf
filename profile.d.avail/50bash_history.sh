9cd51f494c86e8a7e407d9bffa0a48f031a92a41
Guenther Brunthaler <root@xrpi2.emgenxx69lwyn5ctlr4nl64ul.local>
2020-02-19 16:42:51 +0100

    Support .bash_aliases out of the box
    Also add more configuration for history handling.

--- profile.d.avail/50bash_history.sh
+++ profile.d.avail/50bash_history.sh
@@ -1,9 +1,20 @@
 #! /bin/false
-# Version 2019.286
+# Version 2020.50
 
 if test -n "$BASH_VERSION"
 then
 	# Enable time stamps in bash history.
 	export HISTTIMEFORMAT="{%F %T} "
 	shopt -s histreedit histverify
+
+	# don't put duplicate lines or lines starting with space in the
+	# history. See bash(1) for more options
+	HISTCONTROL=ignoreboth
+
+	# append to the history file, don't overwrite it
+	shopt -s histappend
+
+	# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+	export HISTSIZE=1000
+	export HISTFILESIZE=2000
 fi
--- profile.d.avail/95bashrc.sh
+++ profile.d.avail/95bashrc.sh
@@ -3,6 +3,19 @@
 
 if test -n "$BASH_VERSION"
 then
+	# If not running interactively, don't do anything further.
+	case $- in
+	    *i*)
+		# check the window size after each command and, if necessary,
+		# update the values of LINES and COLUMNS.
+		shopt -s checkwinsize
+
+		if test -f ~/.bash_aliases
+		then
+			. ~/.bash_aliases
+		fi
+	esac
+
 	if test -f ~/.bashrc
 	then
 		# Finally, source ~/.bashrc too.
