53abb09ca7598ec7d598fde474bdd2736c6f8213
Guenther Brunthaler <root@xeeepc.emgenxx69lwyn5ctlr4nl64ul.local>
2020-02-19 16:10:22 +0100

    Check more environment variables in reset_env
    For instance, also keep $DISPLAY if it is set.
    
    Unexported or empty variables will be ignored in either case.

--- profile.d.avail/50reset-env.sh
+++ profile.d.avail/50reset-env.sh
@@ -1,9 +1,9 @@
 # Start with a fresh shell environment as if just logged in.
-# Version 2020.50
+# Version 2020.50.1
 
 alias reset-env="`
 	a='exec env -i'
-	for e in TERM HOME LANG LANGUAGE
+	while read e
 	do
 		eval \"ena=\\\${\$e+true}\"
 		eval \"test \\\"\\\$\$e\\\" || ena=\"
@@ -11,7 +11,28 @@ alias reset-env="`
 			'') ;;
 			*) a=\"\$a \$e=\\\"\\\$\$e\\\"\"
 		esac
-	done
+	done <<- EOF
+		DBUS_SESSION_BUS_ADDRESS
+		DISPLAY
+		HOME
+		LANG
+		LANGUAGE
+		LC_ADDRESS
+		LC_ALL
+		LC_COLLATE
+		LC_CTYPE
+		LC_IDENTIFICATION
+		LC_MEASUREMENT
+		LC_MESSAGES
+		LC_MONETARY
+		LC_NAME
+		LC_NUMERIC
+		LC_PAPER
+		LC_TELEPHONE
+		LC_TIME
+		SSH_AUTH_SOCK
+		TERM
+	EOF
 	case \$SHELL in
 		'') unset SHELL;;
 		*) command -v \"\$SHELL\" > /dev/null 2>& 1 || unset SHELL
