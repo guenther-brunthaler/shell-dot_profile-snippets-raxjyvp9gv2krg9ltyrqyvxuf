be9b3a07bd3e5d54b133cefb3ee774e4e9e4a024
Guenther Brunthaler <root@rpi4wb.atehj8v8f827gdu4z2j93pzzw.local>
2022-02-25 18:06:18 +0100

    Make use of pre-existing $XDG_RUNTIME_DIR
    This makes the XDG_RUNTIME_DIR setup snippet work with as well as
    without "logind"/"elogind" and "systemd --user".
    
    It will honor a valid $XDG_RUNTIME_DIR which has been setup before the
    snippet has been run, but will create its own directory for it
    otherwise.
    
    In both cases, a symlink to the same location will also be created
    (unless it already exists with the same target).
    
    If $XDG_RUNTIME_DIR has not been set but the symlink and its target
    already does exist, set XDG_RUNTIME_DIR to the symlink's target rather
    than creating a new temporary directory.

--- profile.d.avail/08xdg_runtime_dir.sh
+++ profile.d.avail/08xdg_runtime_dir.sh
@@ -1,30 +1,54 @@
 #! /bin/sh
 # Set up $XDG_RUNTIME_DIR (unless already set).
 
-# Version 2019.255
-# Copyright (c) 2019 Guenther Brunthaler. All rights reserved.
+# Version 2022.55
+# Copyright (c) 2019-2022 Guenther Brunthaler. All rights reserved.
 #
 # This script is free software.
 # Distribution is permitted under the terms of the GPLv3.
 
-test "$XDG_RUNTIME_DIR" || {
-	XDG_RUNTIME_DIR=${XDG_DATA_HOME:-"$HOME"/.local/share}
-	mkdir -p -- "$XDG_RUNTIME_DIR"
-	XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR/`
-			:
-		`xdg_runtime_dir-ngjjaq42wb5238hup1bmwjtpp.d
-	test -L "$XDG_RUNTIME_DIR" && test -d "$XDG_RUNTIME_DIR" || (
-		set -e
-		if test -L "$XDG_RUNTIME_DIR" || test -e "$XDG_RUNTIME_DIR"
-		then
-			rm -- "$XDG_RUNTIME_DIR"
-		fi
-		u=`id -u`
-		u=`mktemp -d -- "${TMPDIR:-/tmp}/run-$u.XXXXXXXXXX"`
-		ln -s -- "$u" "$XDG_RUNTIME_DIR"
-	) \
-	&& XDG_RUNTIME_DIR=`readlink -- "$XDG_RUNTIME_DIR"` \
-	&& export XDG_RUNTIME_DIR \
-	&& test -d "$XDG_RUNTIME_DIR" && test -w "$XDG_RUNTIME_DIR" \
-	|| unset XDG_RUNTIME_DIR
-}
+L_3qunzp66f5emuf5xvt9avr6qy=${XDG_DATA_HOME:-"$HOME"/.local/share}
+mkdir -p -- "$L_3qunzp66f5emuf5xvt9avr6qy"
+L_3qunzp66f5emuf5xvt9avr6qy=$L_3qunzp66f5emuf5xvt9avr6qy/`
+		:
+	`xdg_runtime_dir-ngjjaq42wb5238hup1bmwjtpp.d
+
+if test "$XDG_RUNTIME_DIR" && test -d "$XDG_RUNTIME_DIR"
+then
+	eq_3qunzp66f5emuf5xvt9avr6qy=maybe
+else
+	if
+		test -L "$L_3qunzp66f5emuf5xvt9avr6qy" \
+		&& test -d "$L_3qunzp66f5emuf5xvt9avr6qy"
+	then
+		XDG_RUNTIME_DIR=`readlink -- "$L_3qunzp66f5emuf5xvt9avr6qy"`
+		eq_3qunzp66f5emuf5xvt9avr6qy=true
+	else
+		XDG_RUNTIME_DIR=`id -u`
+		XDG_RUNTIME_DIR=`mktemp -d -- "${TMPDIR:-/tmp}/run-\`:
+			\`$XDG_RUNTIME_DIR".XXXXXXXXXX`
+		eq_3qunzp66f5emuf5xvt9avr6qy=false
+	fi
+fi
+export XDG_RUNTIME_DIR
+
+if test "$eq_3qunzp66f5emuf5xvt9avr6qy" = maybe
+then
+	if
+		test -L "$L_3qunzp66f5emuf5xvt9avr6qy" \
+		&& test "`readlink -- "$L_3qunzp66f5emuf5xvt9avr6qy"`" \
+			== "$XDG_RUNTIME_DIR"
+	then
+		eq_3qunzp66f5emuf5xvt9avr6qy=true
+	else
+		eq_3qunzp66f5emuf5xvt9avr6qy=false
+	fi
+fi
+if test $eq_3qunzp66f5emuf5xvt9avr6qy = false
+then
+	ln -snf -- "$XDG_RUNTIME_DIR" "$L_3qunzp66f5emuf5xvt9avr6qy"
+fi
+
+test -w "$L_3qunzp66f5emuf5xvt9avr6qy" || unset XDG_RUNTIME_DIR
+
+unset L_3qunzp66f5emuf5xvt9avr6qy eq_3qunzp66f5emuf5xvt9avr6qy
