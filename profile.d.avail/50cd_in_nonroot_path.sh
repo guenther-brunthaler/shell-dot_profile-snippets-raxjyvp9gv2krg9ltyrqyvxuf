d0c7cbf04681bafee24f6547f641aecdb59b58b7
Guenther Brunthaler <root@xrpi2.emgenxx69lwyn5ctlr4nl64ul.local>
2022-04-12 10:34:16 +0200

    Fix broken line continuation in snippet

--- profile.d.avail/50cd_in_nonroot_path.sh
+++ profile.d.avail/50cd_in_nonroot_path.sh
@@ -1,5 +1,5 @@
 #! /bin/false
-# v2022.101
+# v2022.101.1
 #
 # Adds the current directory at the end of the $PATH
 # but only if the current user is not the superuser.
@@ -7,7 +7,7 @@
 if test "$EUID" != 0 && test "$USER" != root
 then
 	set -- \
-		--append .
+		--append . \
 		--stop ${1+"$@"}
 	. ~/.profile.d/shellrc/libexec/path_hyec3v5m8kd1vjs8k7d1wce62.sh
 fi
