ef101e0a473b04c1d20d3b8bc003b7c03dfbe9d9
Guenther Brunthaler <root@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2022-06-15 16:14:33 +0200

    Snippet for traditional scrollbars with GTK3+
    Also read the instructions within the snippet for the remaining required
    steps in order to achive this objective.

---
+++ profile.d.avail/50traditionalgtk3scrollbars.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+export GTK_OVERLAY_SCROLLING=0
+
+# Plus follow the instructions on
+# https://unix.stackexchange.com/questions/542029/how-to-get-traditional-scrollbars-in-gtk-2-and-3-apps-in-debian-10
