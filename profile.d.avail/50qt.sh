50fb00a123b16c55a43a2a87549a430068b10371
Guenther Brunthaler <root@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2023-08-06 21:14:05 +0200

    Provide environent variable required by Qt5
    At least starting with Debian 12, Qt seems to require another
    environment variable to be set or platform themes won't show correctly.

---
+++ profile.d.avail/50qt.sh
@@ -0,0 +1,7 @@
+# Version 2023.218
+# Copyright (c) 2023 Guenther Brunthaler. All rights reserved.
+#
+# This script is free software.
+# Distribution is permitted under the terms of the GPLv3.
+
+export QT_QPA_PLATFORMTHEME=qt5ct
